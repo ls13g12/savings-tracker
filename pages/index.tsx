0a5658cc8f76de19133c4c54521f323fd44033c6
ls13g12 <luke.stoneman.94@gmail.com>
2022-05-03 23:31:57 +0100

    coingecko api for trending coins

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,19 +1,33 @@
 import Head from 'next/head'
 
-const Home = () => {
-  return (
-    <div>
-      <Head>
-        <title>Tracker</title>
-      </Head>
+export default function Home({ coins }){
+    console.log(coins)
+    return (
+        <div>
+        <Head>
+            <title>Tracker</title>
+        </Head>
 
-      <main>
-        <h1>
-          Hello World!
-        </h1>
-      </main>
-    </div>
-  )
+        <main>
+            <h1>Hello World</h1>
+            <div>
+                {coins.map((coin) => (
+                <div key={coin.item.coin_id}> {coin.item.name}: {coin.item.price_btc} BTC </div>
+                ))}
+            </div>
+        </main>
+        </div>
+    )
+ }
+
+
+export const getStaticProps = async () => {
+    const res = await fetch(`https://api.coingecko.com/api/v3/search/trending`)
+    const data = await res.json()
+
+    return {
+        props: {
+            coins: data.coins,
+        },
+    }
 }
-
-export default Home
