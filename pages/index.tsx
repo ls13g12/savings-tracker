ea5eb61b4276a9ccf87a8ea0516bc544eecf0d45
ls13g12 <82527268+ls13g12@users.noreply.github.com>
2022-05-07 12:20:35 +0100

    Revert "coingecko api for trending coins"

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,38 +1,19 @@
 import Head from 'next/head'
-import { InferGetStaticPropsType } from 'next'
 
-export default function Home({ coins }: InferGetStaticPropsType<typeof getStaticProps>){
-    return (
-        <div>
-        <Head>
-            <title>Tracker</title>
-        </Head>
+const Home = () => {
+  return (
+    <div>
+      <Head>
+        <title>Tracker</title>
+      </Head>
 
-        <main>
-            <h1>Hello World</h1>
-            <div>
-                {coins.map((coin: Coin) => (
-                <div key={coin.item.coin_id}> {coin.item.name}: {Number(coin.item.price_btc).toFixed(8)} BTC </div>
-                ))}
-            </div>
-        </main>
-        </div>
-    )
- }
-
-interface Coin {
-    item: {
-        coin_id: number
-        name: string
-        price_btc: string
-    }
+      <main>
+        <h1>
+          Hello World!
+        </h1>
+      </main>
+    </div>
+  )
 }
 
-export const getStaticProps = async () => {
-    const res = await fetch(`https://api.coingecko.com/api/v3/search/trending`)
-    const data = await res.json()
-
-    return{ 
-        props: {coins: data.coins}
-    }
-}
+export default Home
